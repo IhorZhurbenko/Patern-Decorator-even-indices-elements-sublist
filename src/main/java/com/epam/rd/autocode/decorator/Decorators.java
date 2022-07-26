349b8d8891e812f6ae4e8f5e93226474ad1534f1
Igor <igorzhurb@gmail.com>
2022-07-26 16:10:17 +0300

    1

--- src/main/java/com/epam/rd/autocode/decorator/Decorators.java
+++ src/main/java/com/epam/rd/autocode/decorator/Decorators.java
@@ -1,9 +1,46 @@
 package com.epam.rd.autocode.decorator;
 
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Decorators {
     public static List<String> evenIndexElementsSubList(List<String> sourceList) {
-        throw new UnsupportedOperationException();
+        return sourceList.stream().filter(s -> sourceList.indexOf(s) % 2 == 0).collect(Collectors.toList());
+//
+//                return new AbstractList<>(){
+//                    private List<String> list = new ArrayList<>();
+//                    private int size;
+//
+//                    {
+//                        for(int i = 0; i < sourceList.size(); i++){
+//                            if(i % 2 != 0)
+//                                continue;
+//                            list.add(sourceList.get(i));
+//                            size++;
+//                        }
+//                    }
+//
+//                    @Override
+//                    public int size() {
+//                        return size;
+//                    }
+//
+//                    @Override
+//                    public String get(int index) {
+//                        return list.get(index);
+//                    }
+//
+//                    @Override
+//                    public Iterator<String> iterator(){
+//                        return list.iterator();
+//                    }
+//                };
+
+
+        //    return new DecoratorImpl(sourceList);
+
     }
 }
